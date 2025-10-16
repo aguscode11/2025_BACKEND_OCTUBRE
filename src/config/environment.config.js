89327e7a8105e7844c95930cb43fde30cb81e8e0
agus.dev <ruizagustin26@gmail.com>
2025-10-16 02:49:43 -0300

    first commit

---
+++ src/config/environment.config.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+
+
+//Cargar las variables de entorno en process.env
+dotenv.config()
+
+
+//Diccionario donde guando mis variables de entorno
+const ENVIRONMENT = {
+    GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
+    GMAIL_USER: process.env.GMAIL_USER,
+    PORT: process.env.PORT,
+    /* MONGO_DB_HOST: process.env.MONGO_DB_HOST,
+    MONGO_DB_NAME: process.env.MONGO_DB_NAME, */
+    MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING,
+    URL_FRONTEND: process.env.URL_FRONTEND,
+    JWT_SECRET: process.env.JWT_SECRET
+}
+
+
+export default ENVIRONMENT
